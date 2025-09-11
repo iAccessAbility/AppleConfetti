12417c4b3ace99393aa602924dd4e0fca433ab7e
Your Name <your@email.com>
2025-09-11 17:38:46 -0500

    removing event details for Awe Dropping

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('touchstart', handleFirstInteraction);
 
     const mainTitle = document.createElement('h1');
-    mainTitle.textContent = "Awe Dropping";
+    mainTitle.textContent = "No Upcoming Events";
     mainTitle.id = 'mainTitle';
     mainTitle.style.position = 'fixed';
     mainTitle.style.display = 'block';
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
     body.appendChild(mainTitle);
     
     const secondaryText = document.createElement('p');
-    secondaryText.textContent = "September 9 10 AM PT";
+    secondaryText.textContent = "";
     secondaryText.id = 'mainTitle';
     secondaryText.style.position = 'fixed';
     secondaryText.style.display = 'block';
